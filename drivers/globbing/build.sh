f4d5e481e492dd566ae1efd8360c65dc5005c2b6
Molnár Viktor <viktor2.molnar@evosoft.com>
2025-03-14 14:44:38 +0100

    Removing numbers and globbing drivers

--- drivers/globbing/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ../..
-
-cmake --preset globbing-release
-cmake --build --preset globbing-release-build --target install
--- drivers/numbers/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ../..
-
-cmake --preset numbers-release
-cmake --build --preset numbers-release-build --target install
